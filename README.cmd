093d945ca26a20735ece15421253cfafe0a79150
amitknit09 <46901463+amitknit09@users.noreply.github.com>
2022-01-14 01:07:05 +0530

    Update README.cmd

--- README.cmd
+++ README.cmd
@@ -1,7 +1,10 @@
+# run-devops
 # Deploying .Net Microservices to Azure Kubernetes Services(AKS) and Automating with Azure DevOps
+
 | Image | Status |
 | ------------- | ------------- |
 [![Build Status](https://akanaujia.visualstudio.com/shopping/_apis/build/status/shoppingclient-pipeline?branchName=main)]
 (https://akanaujia.visualstudio.com/shopping/_build/latest?definitionId=6&branchName=main)
+
 [![Build Status](https://akanaujia.visualstudio.com/shopping/_apis/build/status/shoppingapi-pipeline?branchName=main)]
 (https://akanaujia.visualstudio.com/shopping/_build/latest?definitionId=5&branchName=main)
